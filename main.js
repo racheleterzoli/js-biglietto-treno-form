d70508473fdc5f57fe5d3a2a51ff0f115e675c9b
rachele <rachele.terzoli@gmail.com>
2020-03-28 14:45:11 +0100

    macrostruttura

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // finestra introduttiva
-alert("Buongiorno e benvenuti in TreniTreni, le farò qualche domanda per calcolare il prezzo del suo biglietto. In base al suo chilometraggio e  alla sua età avrà il suo prezzo personalizzato. La ringrazio per averci scelti. Prema ok per continuare ");
+// alert("Buongiorno e benvenuti in TreniTreni, le farò qualche domanda per calcolare il prezzo del suo biglietto. In base al suo chilometraggio e  alla sua età avrà il suo prezzo personalizzato. La ringrazio per averci scelti. Prema ok per continuare ");
 // creare variabile nome e cognome
 var identity;
 // creare variabile km
@@ -9,11 +9,11 @@ var age;
 // creare variabile costo km senza sconto
 var costoperkm;
 // chiedere il nome e il cognome
-identity = prompt("Inserisca nome e cognome per favore.");
+// identity = prompt("Inserisca nome e cognome per favore.");
 // chiedere il kilometraggio
-km = prompt ("inserire il numero di chilometri che vuole fare");
+// km = prompt ("inserire il numero di chilometri che vuole fare");
 // chiedere l'età
-age = prompt ("inserisca la sua età per favore");
+// age = prompt ("inserisca la sua età per favore");
 // moltiplicare il numero di km x 0.21 euro
 costoperkm = (km * 0.21);
 // controllare eta se maggiore di 65 o minore di 18
